e2ef3911bee0ddd7bb36d91384d2faf498c201e1
somin1116 <sominjung1116@gmail.com>
2025-06-27 17:33:15 +0900

    oo

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <form class="login-container">
